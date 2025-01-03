beacb605e3af760d52c0a2171e669d1f7ff47a9d
EsraaFathi <138526584+EsraaFathi@users.noreply.github.com>
2025-01-03 10:37:44 +0200

    login form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <div className="text-white">eeeeeeeeeeeeee</div>;
+  return (
+    <div className="flex h-screen max-h-screen">
+      {/* OTP Verification|Passkey model */}
+      <section className=" remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            height={1000}
+            width={1000}
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+          <div className=" text-14-regular mt-10 flex justify-between ">
+            <p className="text-dark-600 justify-items-end xl:text">
+              © 2024 CarePlus
+            </p>
+            <Link href="/?admin=true" className="text-green-500"></Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="patien"
+        height={1000}
+        width={1000}
+        className="side-img max-w-[50%]"
+      />
+    </div>
+  );
 }
